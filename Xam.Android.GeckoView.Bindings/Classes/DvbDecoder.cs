ba650311de371ec2b54bcef6ca622d0f519fe9fa
Guillaume ZAHRA <gzahra@2bee.io>
2019-02-26 18:12:49 +0100

    Registered bindings for DvbDecoder and DvbSubtitle

---
+++ Xam.Android.GeckoView.Bindings/Classes/DvbDecoder.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Com.Google.Android.Exoplayer2.Text.Dvb
+{
+    public sealed partial class DvbDecoder
+    {
+        protected override ISubtitle Decode(byte[] p0, int p1, bool p2)
+        {
+            return DecodeDvbSubtitle(p0, p1, p2);
+        }
+
+        protected override Java.Lang.Object Decode(Java.Lang.Object p0, Java.Lang.Object p1, bool p2)
+        {
+            //DvbSubtitle in ISubtitle interface actually inherit from Java.Lang.Object
+            return (global::Java.Lang.Object)((ISubtitle)Decode(p0, p1, p2));
+        }
+
+        protected override Java.Lang.Object CreateInputBuffer()
+        {
+            return CreateInputBufferBase();
+        }
+
+        protected override Java.Lang.Object CreateOutputBuffer()
+        {
+            return CreateOutputBufferBase();
+        }
+    }
+}
