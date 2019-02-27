49ea1ab05ad03f92329e0a28aa5dddf0b72f33ae
Guillaume ZAHRA <gzahra@2bee.io>
2019-02-27 15:36:38 +0100

    Added more type

---
+++ Xam.Android.GeckoView.Bindings/Classes/CeaOutputBuffer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Google.Android.Exoplayer2.Text.Cea
+{
+    public sealed partial class CeaOutputBuffer
+    {
+        public unsafe override void ReleaseSubtitleOutputBuffer()
+        {
+            const string __id = "release.()V";
+            try
+            {
+                _members.InstanceMethods.InvokeNonvirtualVoidMethod(__id, this, null);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
---
+++ Xam.Android.GeckoView.Bindings/Classes/DefaultHttpDataSourceFactory.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Com.Google.Android.Exoplayer2.Upstream
+//{
+//    public sealed partial class DefaultHttpDataSourceFactory
+//    {
+//        public override IDataSource CreateDataSource()
+//        {
+//            return CreateDataSourceBase();
+//        }
+
+//        protected override IHttpDataSource CreateDataSourceInternal(HttpDataSourceRequestProperties p0)
+//        {
+//            return CreateDataSourceInternalBase(p0);
+//        }
+//    }
+//}
